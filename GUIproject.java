09c470a8bb37ff3f95582065a1c8ee974abfb221
EmmaLynch1 <163864134+EmmaLynch1@users.noreply.github.com>
2024-03-22 09:35:35 -0400

    test

--- GUIproject.java
+++ GUIproject.java
@@ -1,2 +1,16 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class GUIproject {
+    private JButton button1;
+
+    public GUIproject() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                
+            }
+        });
+    }
 }
