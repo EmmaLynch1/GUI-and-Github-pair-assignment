cd996dba3879df1efba33a3a0c07398a62c7d4bf
EmmaLynch1 <163864134+EmmaLynch1@users.noreply.github.com>
2024-03-22 09:45:07 -0400

    test1

--- GUIproject.java
+++ GUIproject.java
@@ -4,9 +4,16 @@ import java.awt.event.ActionListener;
 
 public class GUIproject {
     private JButton button1;
+    private JButton button2;
 
     public GUIproject() {
         button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        button2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 
