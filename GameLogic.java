eee36ff01e15fa3c1576394dd25f70988dfc3425
bbmmccggii <163865084+bbmmccggii@users.noreply.github.com>
2024-03-26 10:06:36 -0400

    Add files via upload

--- GameLogic.java
+++ GameLogic.java
@@ -1,31 +1,29 @@
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Random;
 
 class GameLogic {
     String[] words = {"GIRAFFE", "ARCTIC", "HANGMAN", "SUPERCALIFRAGILISTICEXPIALIDOCIOUS", "XENOTRANSPLANTATION", "ENGLISH", "BASKETBALL", "GENERATION"};
     String chosenWord;
 
-    String generateWord() {
+    static String generateWord() {
         Random rand = new Random();
-        int max = words.length;
+        int max = words.length - 1;
         int min = 0;
 
         int randomNumber = rand.nextInt((max - min) + 1) + min; // Generate number from a min of 0 to a max of the words array
-
+        
         chosenWord = words[randomNumber]; // Take random number and put it as the word that the main game will use
 
         return chosenWord;
     }
 
     // I am going to make this more elaborate later + not fully done
-    boolean compareInputToChosenWord(String input) {
-        if (chosenWord.contains(input)) {
+    static boolean compareInputToChosenWord(String input, String hiddenWord) {
+        if (hiddenWord.contains(input)) {
             return true;
         }
         else {
             return false;
         }
+
     }
 }
