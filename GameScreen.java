f0982f732afaed546c2f442fb4a19a653f786456
EmmaLynch1 <163864134+EmmaLynch1@users.noreply.github.com>
2024-03-29 11:39:39 -0400

    Add files via upload

--- GameScreen.java
+++ GameScreen.java
@@ -1,2 +1,22 @@
-public class GameScreen {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameScreen extends JFrame {
+    private JButton mainPopup;
+    private JPanel GameScreen;
+
+    public GameScreen() {
+        setTitle("Hangman");
+        setSize(400, 300);
+        setBounds(400, 100, 600, 500);
+        mainPopup.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        setContentPane(GameScreen);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
 }
