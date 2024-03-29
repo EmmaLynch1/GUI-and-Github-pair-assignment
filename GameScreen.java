0b30ed31a62bc663aa4c3f439702a7888010ce74
EmmaLynch1 <163864134+EmmaLynch1@users.noreply.github.com>
2024-03-29 12:29:03 -0400

    Add files via upload
    Set up page for game screen- buttons are alligned properly

--- GameScreen.java
+++ GameScreen.java
@@ -5,11 +5,38 @@ import java.awt.event.ActionListener;
 public class GameScreen extends JFrame {
     private JButton mainPopup;
     private JPanel GameScreen;
+    private JButton vButton;
+    private JButton hButton;
+    private JButton wButton;
+    private JButton xButton;
+    private JButton pButton;
+    private JButton qButton;
+    private JButton bButton1;
+    private JButton cButton1;
+    private JButton dButton1;
+    private JButton eButton1;
+    private JButton fButton1;
+    private JButton gButton;
+    private JButton aButton1;
+    private JButton kButton;
+    private JButton jButton;
+    private JButton iButton;
+    private JButton lButton;
+    private JButton mButton;
+    private JButton nButton;
+    private JButton oButton;
+    private JButton yButton;
+    private JButton rButton;
+    private JButton sButton;
+    private JButton tButton;
+    private JButton uButton;
+    private JButton zButton;
 
     public GameScreen() {
         setTitle("Hangman");
         setSize(400, 300);
         setBounds(400, 100, 600, 500);
+
         mainPopup.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -19,4 +46,5 @@ public class GameScreen extends JFrame {
         setContentPane(GameScreen);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
+
 }
