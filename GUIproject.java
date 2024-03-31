e4adc4dee9e0113dcec3ab79ddc89f38a7a8644c
EmmaLynch1 <163864134+EmmaLynch1@users.noreply.github.com>
2024-03-31 14:21:36 -0400

    Add files via upload
    functioning game- all thats left it handling incorrect letters ( body parts left)

--- GUIproject.java
+++ GUIproject.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,38 +7,26 @@ public class GUIproject extends JFrame {
     private JButton instructionsPopup;
     private JButton exitButton;
     private JPanel homeScreen;
-    private JLabel mainPic;
-
 
     public GUIproject() {
         playButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                GameScreen play=new GameScreen();
-                play.setVisible(true);//open game form
-                dispose();//close current
+
             }
         });
         instructionsPopup.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                //open instructions frame
-                Instructions popup=new Instructions();
-                popup.setVisible(true);
-
 
             }
         });
         exitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                int result = JOptionPane.showConfirmDialog(null, "Are you sure you want to quit?", "confirm", JOptionPane.YES_NO_OPTION);
-                if (result == JOptionPane.YES_OPTION) {
-                    System.exit(0);
-                }
+
             }
         });
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
 
     public static void main(String[] args) {
@@ -51,8 +38,6 @@ public class GUIproject extends JFrame {
         h.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-        mainPic=new JLabel(new ImageIcon("main.png"));
-    }
+
 }
+
--- Instructions.java
+++ Instructions.java
@@ -1,27 +1,3 @@
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class Instructions extends JFrame{
-    private JButton GOBACKButton;
-    private JPanel instructions;
-
-
-    public Instructions() {
-        setTitle("Instructions");
-        setSize(400, 300);
-        setBounds(400, 100, 600, 500);
-        GOBACKButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                dispose();
-            }
-        });
-        setContentPane(instructions);
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
-    }
-    }
-
-
+public class Instructions {
+}
