0aa4735cd2d7dc333db8824872feac013b36b8e1
EmmaLynch1 <163864134+EmmaLynch1@users.noreply.github.com>
2024-04-01 16:23:32 -0400

    Add files via upload
    properly displayed remaining guesses

--- GameLogic.java
+++ GameLogic.java
@@ -1,34 +1,4 @@
 import java.util.Random;
-/*
-class GameLogic {
-    String[] words = {"GIRAFFE", "ARCTIC", "HANGMAN", "SUPERCALIFRAGILISTICEXPIALIDOCIOUS", "XENOTRANSPLANTATION", "ENGLISH", "BASKETBALL", "GENERATION"};
-    String chosenWord;
-
-    static String generateWord() {
-        Random rand = new Random();
-        int max = words.length - 1;
-        int min = 0;
-
-        int randomNumber = rand.nextInt((max - min) + 1) + min; // Generate number from a min of 0 to a max of the words array
-        
-        chosenWord = words[randomNumber]; // Take random number and put it as the word that the main game will use
-
-        return chosenWord;
-    }
-
-    // I am going to make this more elaborate later + not fully done
-    static boolean compareInputToChosenWord(String input, String hiddenWord) {
-        if (hiddenWord.contains(input)) {
-            return true;
-        }
-        else {
-            return false;
-        }
-
-    }
-}
-*/
-
 public class GameLogic {
 
     private String chosenWord;
@@ -84,6 +54,7 @@ public class GameLogic {
         } else {
             //incorrect guess
             remainingGuesses--;
+            gameScreen.updateRemainingGuessesLabel(getRemainingGuesses());
             return false;
         }
     }
